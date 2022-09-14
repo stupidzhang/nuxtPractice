6520462c29846370785c8596cd9386a1c66cde95
zhangjiyue <jinyue@yscredit.com>
2022-09-14 11:00:29 +0800

    feat: element theme

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["element-ui/lib/theme-chalk/index.css"],
+  css: ["element-ui/lib/theme-chalk/index.css",'@/assets/css/normailze.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["@/plugins/element-ui", "@/plugins/route"],
